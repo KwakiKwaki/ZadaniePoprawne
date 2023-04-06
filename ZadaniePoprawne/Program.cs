009215fb069aa42cd22d61ebe1fed8977ff58d71
Sławomir <szachownicawroclaw@gmail.com>
2023-04-06 19:28:19 +0200

    Zadanie Poprawne

---
+++ ZadaniePoprawne/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Design;
+
+string name = "Monika";
+string sex = "Kobieta";
+var age = 15;
+
+if (sex == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta przed 30");
+}
+else if (name == "Monika" && age == 33)
+{
+    Console.WriteLine("Monika lat 33");
+}
+else if (age < 18 && sex != "Kobieta")
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
